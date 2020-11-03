3af8d8a2af9ff86faba48e5afa4ac11a650ad58f
asadghaffar <38716939+asadghaffar@users.noreply.github.com>
2020-11-03 16:25:47 +0500

    Web View

--- app/src/main/java/com/asda/task/MainActivity.java
+++ app/src/main/java/com/asda/task/MainActivity.java
@@ -20,10 +20,7 @@ import com.karumi.dexter.Dexter;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_CODE_CAMERA = 1000;
     private WebView webView;
-    private Button cameraButton;
-    private Button microphoneButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
 
         id();
         webViewMethod();
-        onclick();
 
     }
 
@@ -43,39 +39,7 @@ public class MainActivity extends AppCompatActivity {
         webView.loadUrl("https://www.google.com/");
     }
 
-    private void onclick() {
-        cameraButton.setOnClickListener(view ->
-                ActivityCompat.requestPermissions(
-                        MainActivity.this,
-                        new String[]{Manifest.permission.CAMERA},
-                        REQUEST_CODE_CAMERA
-                ));
-    }
-
-    // Open camera method
-    private void openCamera() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, REQUEST_CODE_CAMERA);
-    }
-
     private void id() {
-        webView = findViewById(R.id.webview);
-        cameraButton = findViewById(R.id.camera);
-        microphoneButton = findViewById(R.id.microphone);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_CODE_CAMERA) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                openCamera();
-
-            } else {
-                Toast.makeText(getApplicationContext(), "You don't have permission to access camera!", Toast.LENGTH_SHORT).show();
-            }
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
+        webView = findViewById(R.id.webview);}
 
 }
